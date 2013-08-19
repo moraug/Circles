71a9421cda5cec7875a8c6a1a2e67bfb209d28d0
moraug <moraug@gmail.com>
2013-08-20 00:43:36 +0300

    right/left

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ var canvas = document.getElementById("canvas");
 canvas.width = 0.9 * window.innerWidth;
 canvas.height = 0.9 * window.innerHeight;
 var ctx = canvas.getContext("2d");
+initDraw();
 var targetX = undefined, targetY = undefined;
 var flag = false;
 
 canvas.addEventListener("click", function (e) {
-	if (targetX == undefined || targetY == undefined) {
+	if ((targetX == undefined || targetY == undefined) && e.clientX <= canvas.width / 2 -25 ) {
 		targetX = e.clientX;
 		targetY = e.clientY;
 		drawCircle(targetX, targetY);
@@ -21,7 +22,7 @@ canvas.addEventListener("mousedown", function (e) {
 
 canvas.addEventListener("mousemove", function (e) {
 	if (flag) {
-		ctx.clearRect(0, 0, canvas.width / 2 - 1, canvas.height);
+		ctx.clearRect(0, 0, canvas.width / 2, canvas.height);
 		targetX = e.clientX <= canvas.width / 2 - 25 ? e.clientX : canvas.width / 2 - 25;
 		targetY = e.clientY <= canvas.height ? e.clientY : canvas.height;
 		drawCircle(targetX, targetY);
@@ -35,5 +36,14 @@ canvas.addEventListener("mouseup", function (e) {
 function drawCircle(targetX, targetY) {
 	ctx.beginPath();
 	ctx.arc(targetX, targetY, 20, 0, 2 * Math.PI);
+	ctx.moveTo(canvas.width / 2, 0);
+	ctx.lineTo(canvas.width / 2, canvas.height);
+	ctx.stroke();
+}
+
+function initDraw(){
+	ctx.beginPath();
+	ctx.moveTo(canvas.width / 2, 0);
+	ctx.lineTo(canvas.width / 2, canvas.height);
 	ctx.stroke();
 }
